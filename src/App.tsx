68d98980876362c0c014e0d30ab9fafabb9e2b65
Salman-Sheikh-Muslim <salmansheikh787work@gmail.com>
2024-01-26 21:25:26 +0500

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      {/* templateAreas={`"nav nav" "aside main"`}
+    Defining 2 rows having 2 columns.
+    */}
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        {/* //wider than 1024px */}
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        {/* By using show we are specifying that Aside should only appear in large and above devices */}
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
