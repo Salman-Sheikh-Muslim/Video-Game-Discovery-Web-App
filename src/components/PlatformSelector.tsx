5c02c7f13c244d99e26f8cedd7959b718f757f83
Salman-Sheikh-Muslim <salmansheikh787work@gmail.com>
2024-01-28 00:02:22 +0500

    Built Platform Selector

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,26 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import React from "react";
+import { BsChevronBarDown } from "react-icons/bs";
+import usePlatforms from "../Hooks/usePlatforms";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+
+  return (
+    //Menu chakra defined component can be used to render a beauiful drop down list.
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
+        Platforms
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
