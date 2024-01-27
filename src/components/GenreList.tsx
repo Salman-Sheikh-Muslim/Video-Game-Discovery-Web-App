8b6618907a7fec9045eb25f764f4ebea09ee939b
Salman-Sheikh-Muslim <salmansheikh787work@gmail.com>
2024-01-27 21:47:17 +0500

    Showing spinner/skeleton for genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,26 +1,54 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../Hooks/useGenres";
 import getCroppedImageURL from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 const GenreList = () => {
   //   const { genres } = useGenres();
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  if (error) return null;
+  //if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      {/* List chakra component renders a list without bullet points. */}
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageURL(genre.image_background)}
-            />
-            <Text fontSize="lg">{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <>
+            <List>
+              <ListItem key={skeleton} paddingY="5px">
+                <GenreListSkeleton />
+              </ListItem>
+            </List>
+          </>
+        ))}
+
+      <List>
+        {/* List chakra component renders a list without bullet points. */}
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageURL(genre.image_background)}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,14 @@
+import { HStack, Skeleton, SkeletonText, Spinner } from "@chakra-ui/react";
+import React from "react";
+
+const GenreListSkeleton = () => {
+  return (
+    <HStack>
+      <Spinner />
+      <Skeleton boxSize="32px" borderRadius={8}></Skeleton>
+      <SkeletonText />
+    </HStack>
+  );
+};
+
+export default GenreListSkeleton;
