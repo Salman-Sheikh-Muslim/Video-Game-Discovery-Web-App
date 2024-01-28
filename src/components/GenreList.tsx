2654e7698ea123a5b535880e348722147a6183fc
Salman-Sheikh-Muslim <salmansheikh787work@gmail.com>
2024-01-28 14:43:46 +0500

    Clean up the Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -26,17 +27,20 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (error) return null;
   if (isLoading) return <Spinner />;
 
-  return (
-    <>
-      {/* {isLoading &&
+  /* {isLoading &&
         skeletons.map((skeleton) => (
           <List>
-            <ListItem key={skeleton} paddingY="5px">
-              <GenreListSkeleton />
-            </ListItem>
+          <ListItem key={skeleton} paddingY="5px">
+          <GenreListSkeleton />
+          </ListItem>
           </List>
-        ))} */}
+        ))} */
 
+  return (
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {/* List chakra component renders a list without bullet points. */}
         {data.map((genre) => (
@@ -45,9 +49,12 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
               <Image
                 boxSize="32px"
                 borderRadius={8}
+                objectFit="cover"
                 src={getCroppedImageURL(genre.image_background)}
               />
               <Button
+                whiteSpace="normal"
+                textAlign="left"
                 fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 fontSize="lg"
