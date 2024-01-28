c2ac60830988dea5073fb5a47fa3d6f959c0e1f4
Salman-Sheikh-Muslim <salmansheikh787work@gmail.com>
2024-01-28 16:08:26 +0500

    Ship genres with the app

--- src/Hooks/useGenres.ts
+++ src/Hooks/useGenres.ts
@@ -1,10 +1,16 @@
 import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
-const useGenres = () => useData<Genre>("/genres");
+// const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({data: genres, isLoading: false, error: null});
+/*
+THe reason we should return an object with these properties is to minimize the impact
+of these changes on the consumer of this hook.
+*/
 
 export default useGenres;
