4af18b7fcc6d7ce9fd39ce8ebfe67d1f43f06b28
Salman-Sheikh-Muslim <salmansheikh787work@gmail.com>
2024-01-28 14:53:33 +0500

    Clean Up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -15,15 +15,15 @@ const GameCard = ({ game }: Props) => {
       <Image src={getCroppedImageURL(game.background_image)} />
       <CardBody>
         {/* fonts size defined 2xl and so on defined Chakra */}
-        <Heading fontSize="2xl">{game.name}</Heading>
         {/* {game.parent_platforms.map(platform => <Text>{platform.platform.name}</Text>)} */}
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           {/* With this mapping we are constructing an array of platform objects. */}
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
