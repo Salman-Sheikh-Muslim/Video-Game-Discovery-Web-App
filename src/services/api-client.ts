2cbd517933ef210f1119e647fa1088e5aacab602
Salman-Sheikh-Muslim <salmansheikh787work@gmail.com>
2024-02-01 19:21:54 +0500

    using dotenv

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,27 @@
 //we need to create an axsios instance with custom configuration in that configuration we are going to include the API key provide by RAWG website.
-
-
 import axios from "axios"
+
+// const apiKey = import.meta.env.VITE_REACT_APP_RAWG_API_KEY;
+// const apiBaseURL = import.meta.env.VITE_REACT_APP_API_BASE_URL;
+
+
+// if (!apiKey) {
+//   throw new Error('API key not found. Make sure to set it in your .env file.');
+// }
+
+// if (!apiBaseURL) {
+//     throw new Error('API base URL not found. Make sure to set it in your .env file.');
+//  }
+  
 
 //Creating an axios instance with custom configuration.
 export default axios.create({ //exporting axios instance as a default object.
+    // baseURL: apiBaseURL,
+
     baseURL: 'https://api.rawg.io/api',
     params:{
-        key: '8cf768320b944bb4a42083e8f82a785d'
+        key:  import.meta.env.VITE_REACT_APP_RAWG_API_KEY,
+        
     }
 })
 /*
