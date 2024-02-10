e78668d54e6d822be6e9c6c14962d258ff7e7afa
Salman-Sheikh-Muslim <salmansheikh787work@gmail.com>
2024-02-10 21:21:17 +0500

    Made the Whole card clickable

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -21,30 +21,32 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
-      <Image src={getCroppedImageURL(game.background_image)} />
-      <CardBody minHeight="100%">
-        {/* <CardBody minHeight="100%" bgColor="#202020" textColor="white"> */}
-        {/* fonts size defined 2xl and so on defined Chakra */}
-        {/* {game.parent_platforms.map(platform => <Text>{platform.platform.name}</Text>)} */}
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          {/* With this mapping we are constructing an array of platform objects. */}
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading
-          fontSize="2xl"
-          overflow="hidden"
-          textOverflow="ellipsis"
-          whiteSpace="nowrap"
-        >
-          <Link to={"/games/" + game.slug}>{game.name}</Link>
-          <Emoji rating={game.rating_top} />
-        </Heading>
-      </CardBody>
-    </Card>
+    <Link to={"/games/" + game.slug}>
+      <Card>
+        <Image src={getCroppedImageURL(game.background_image)} />
+        <CardBody minHeight="100%">
+          {/* <CardBody minHeight="100%" bgColor="#202020" textColor="white"> */}
+          {/* fonts size defined 2xl and so on defined Chakra */}
+          {/* {game.parent_platforms.map(platform => <Text>{platform.platform.name}</Text>)} */}
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            {/* With this mapping we are constructing an array of platform objects. */}
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading
+            fontSize="2xl"
+            overflow="hidden"
+            textOverflow="ellipsis"
+            whiteSpace="nowrap"
+          >
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
